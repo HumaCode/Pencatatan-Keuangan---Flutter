80cf8b4b8c48bfb54e0381e882aa5aede55d4ffa
humaidi <humaidizakaria1998@gmail.com>
2023-01-13 10:49:10 +0700

    implementasi api history & membuat controller home

---
+++ lib/data/source/source_history.dart
@@ -0,0 +1,32 @@
+import 'package:course_money_record/config/api.dart';
+import 'package:course_money_record/config/api_request.dart';
+import 'package:intl/intl.dart';
+
+class SourceHistory {
+  // function analisis
+  static Future<Map> analysis(String idUser) async {
+    // url
+    String url = '${Api.history}/login.php';
+
+    // panggil request post, dibuat dalam bentuk map
+    Map? responseBody = await AppRequest.post(url, {
+      'id_user': idUser,
+      'today': DateFormat('yyyy-MM-dd').format(DateTime.now()),
+    });
+
+    // jika responseBody null
+    if (responseBody == null) {
+      return {
+        'today': 0,
+        'yesterday': 0,
+        'week': [0, 0, 0, 0, 0, 0, 0],
+        'month': {
+          'income': 0,
+          'outcome': 0,
+        }
+      };
+    }
+
+    return responseBody;
+  }
+}
---
+++ lib/presentation/controller/c_home.dart
@@ -0,0 +1,28 @@
+import 'package:course_money_record/data/source/source_history.dart';
+import 'package:get/get.dart';
+
+class CHome extends GetxController {
+  final _today = 0.0.obs;
+  double get today => _today.value;
+
+  final _todayPercent = '0'.obs;
+  String get todayPercent => _todayPercent.value;
+
+  getAnalisis(String idUser) async {
+    Map data = await SourceHistory.analysis(idUser);
+    _today.value = data['today'];
+
+    // ambil nilai yesterday
+    double yesterday = data['yesterday'];
+    double different = (today - yesterday).abs();
+    bool isSame = today.isEqual(yesterday);
+    bool isPlus = today.isGreaterThan(yesterday);
+    double byYesterday = yesterday == 0 ? 1 : yesterday;
+    double percent = (different / byYesterday) * 100;
+    _todayPercent.value = isSame
+        ? '100% sama dengan kemarin'
+        : isPlus
+            ? '+${percent.toStringAsFixed(1)}% dibanding kemarin'
+            : '-${percent.toStringAsFixed(1)}% dibanding kemarin';
+  }
+}
